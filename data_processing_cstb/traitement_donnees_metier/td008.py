5d429cf7896daa549cae1e5f14121e6ec09b78f1
BREITWILLER <antoine.breitwiller@cstb.fr>
2020-05-22 17:20:30 +0200

    adding processing td008 baie orientation & fen_lib

--- data_processing_cstb/traitement_donnees_metier/td008.py
+++ data_processing_cstb/traitement_donnees_metier/td008.py
@@ -58,3 +58,48 @@ td008_types = {'id': 'object',
                'tv020_Orientation de la paroi': 'category',
                'tv020_Type de baie': 'category',
                'tv020_C1': 'category'}
+
+
+def post_processing_td008(td008):
+    from .assets_orm import DPEMetaData
+    meta = DPEMetaData()
+    table = td008
+    table = meta.merge_all_tr_table(table)
+
+    table = meta.merge_all_tv_table(table)
+    table = table.astype(td008_types)
+
+    # orientation processing from tv020 and name.
+    table['orientation_infer'] = table['tv020_Orientation de la paroi'].astype('string').fillna('NONDEF')
+    nondef = table.orientation_infer == 'NONDEF'
+    horiz = table['tv020_coefficient_orientation_id'] == "TV020_013"
+    table.loc[horiz, 'orientation_infer'] = 'Horizontale'
+    ouest = table.reference.str.lower().str.contains('ouest')
+    nord = table.reference.str.lower().str.contains('nord')
+    sud = table.reference.str.lower().str.contains('sud')
+    est = table.reference.str.lower().str.contains('est')
+    table.loc[nord & nondef, 'orientation_infer'] = "Nord"
+    table.loc[sud & nondef, 'orientation_infer'] = "Sud"
+    table.loc[ouest & nondef, 'orientation_infer'] = "Ouest"
+    table.loc[est & nondef, 'orientation_infer'] = "Est"
+    table.loc[(ouest & est & nondef), 'orientation_infer'] = "Est ou Ouest"
+    table.orientation_infer = table.orientation_infer.astype('category')
+
+
+    # fen lib
+
+    table['fen_lib_from_tv009'] = table['tv009_Type de vitrage'].astype('string') + ' ' + table[
+        'tv009_Remplissage'].astype('string').fillna('') + ' '
+    table['fen_lib_from_tv009'] += table[
+                                       'tv009_Epaisseur Lame'].astype('Int32').fillna(0).astype(int).astype(
+        str).replace('0', '').apply(
+        lambda x: x + ' mm ' if x != '' else x) + table['tv010_Type de matériaux'].astype('string').fillna('') + ' ' + \
+                                   table[
+                                       'tv009_Traitement du vitrage'].astype('string').fillna('')
+    table['fen_lib_from_tv009'] = table['fen_lib_from_tv009'].fillna('NONDEF')
+
+    table['fen_lib_from_tv021'] = table['tv021_Type de Baie'].astype('string') + ' ' + table[
+        'tv021_Type de Vitrage'].astype('string').fillna('') + ' '
+    table['fen_lib_from_tv021'] += table['tv021_Matériaux'].astype('string').fillna('')
+    table['fen_lib_from_tv021'] = table['fen_lib_from_tv021'].fillna('NONDEF')
+
