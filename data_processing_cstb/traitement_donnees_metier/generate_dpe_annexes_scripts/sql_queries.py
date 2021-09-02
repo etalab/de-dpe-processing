dd550522a56ead18e2a32abeff7d0d21782c1c3e
BREITWILLER <antoine.breitwiller@cstb.fr>
2021-09-02 17:45:40 +0200

    add sql queries to migrate from csv processing to sql

---
+++ data_processing_cstb/traitement_donnees_metier/generate_dpe_annexes_scripts/sql_queries.py
@@ -0,0 +1,30 @@
+schema_name = "adedpe202006"
+
+def get_td007(dept, schema_name=schema_name):
+    query = f"""
+        SELECT td007_paroi_opaque.*,td006_batiment_id,td001_dpe_id 
+        from {schema_name}.td007_paroi_opaque as  td007_paroi_opaque
+        INNER JOIN {schema_name}.td006_batiment as td006_batiment
+                    ON td006_batiment.id = td007_paroi_opaque.td006_batiment_id
+        INNER JOIN {schema_name}.td001_dpe as td001_dpe
+                    ON td001_dpe.id = td006_batiment.td001_dpe_id
+        WHERE td001_dpe.tv016_departement_id = {dept}
+        """
+    table = pd.read_sql(query, engine)
+    return table
+
+
+def get_td008(dept, schema_name=schema_name):
+    query = f"""
+        SELECT td008_baie.*,td007_paroi_opaque_id,td006_batiment_id,td001_dpe_id 
+        from {schema_name}.td008_baie as  td008_baie
+        INNER JOIN {schema_name}.td007_paroi_opaque as td007_paroi_opaque
+            ON td007_paroi_opaque.id = td008_baie.td007_paroi_opaque_id
+        INNER JOIN {schema_name}.td006_batiment as td006_batiment
+                    ON td006_batiment.id = td007_paroi_opaque.td006_batiment_id
+        INNER JOIN {schema_name}.td001_dpe as td001_dpe
+                    ON td001_dpe.id = td006_batiment.td001_dpe_id
+        WHERE td001_dpe.tv016_departement_id = {dept}
+        """
+    table = pd.read_sql(query, engine)
+    return table
