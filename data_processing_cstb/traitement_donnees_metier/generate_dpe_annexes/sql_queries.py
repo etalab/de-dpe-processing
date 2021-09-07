df120158afff13fd3257b4e2cb2d9d788815d003
BREITWILLER <antoine.breitwiller@cstb.fr>
2021-09-07 13:26:07 +0200

    refactor sql

--- data_processing_cstb/traitement_donnees_metier/generate_dpe_annexes/sql_queries.py
+++ data_processing_cstb/traitement_donnees_metier/generate_dpe_annexes/sql_queries.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 schema_name = "adedpe202006"
-
+td001_cols = 'td001_dpe.td001_dpe_id as td001_dpe_id,td001_dpe.annee_construction as annee_construction,td001_dpe.tv016_departement_id as tv016_departement_id'
 
 def convert_id_column(table, col):
     table[col] = table[col].astype(dtype=pd.Int32Dtype()).astype(str).replace('<NA>', np.nan).astype('category')
@@ -16,7 +16,7 @@ def convert_all_tr_tv_ids(table):
 
 def get_td006(dept, engine, schema_name=schema_name):
     query = f"""
-        SELECT td006_batiment.*,td001_dpe.annee_construction as annee_construction
+        SELECT td006_batiment.*,{td001_cols}
         from {schema_name}.td006_batiment as  td006_batiment
         INNER JOIN {schema_name}.td001_dpe as td001_dpe
                     ON td001_dpe.id = td006_batiment.td001_dpe_id
@@ -30,7 +30,7 @@ def get_td006(dept, engine, schema_name=schema_name):
 
 def get_td007(dept, engine, schema_name=schema_name):
     query = f"""
-        SELECT td007_paroi_opaque.*,td001_dpe_id,td001_dpe.annee_construction as annee_construction
+        SELECT td007_paroi_opaque.*,td006_batiment_id,{td001_cols}
         from {schema_name}.td007_paroi_opaque as  td007_paroi_opaque
         INNER JOIN {schema_name}.td006_batiment as td006_batiment
                     ON td006_batiment.id = td007_paroi_opaque.td006_batiment_id
@@ -47,7 +47,7 @@ def get_td007(dept, engine, schema_name=schema_name):
 def get_td008(dept, engine, schema_name=schema_name):
 
     query = f"""
-        SELECT td008_baie.*,td006_batiment_id,td001_dpe_id,td001_dpe.annee_construction as annee_construction
+        SELECT td008_baie.*,td007_paroi_opaque_id,td006_batiment_id,{td001_cols}
         from {schema_name}.td008_baie as  td008_baie
         INNER JOIN {schema_name}.td007_paroi_opaque as td007_paroi_opaque
             ON td007_paroi_opaque.id = td008_baie.td007_paroi_opaque_id
@@ -67,7 +67,7 @@ def get_td008(dept, engine, schema_name=schema_name):
 
 def get_td010(dept, engine, schema_name=schema_name):
     query = f"""
-        SELECT td010_pont_thermique.*,td001_dpe_id,td001_dpe.annee_construction as annee_construction
+        SELECT td010_pont_thermique.*,td006_batiment_id,{td001_cols}
         from {schema_name}.td010_pont_thermique as  td010_pont_thermique
         INNER JOIN {schema_name}.td006_batiment as td006_batiment
                     ON td006_batiment.id = td010_pont_thermique.td006_batiment_id
