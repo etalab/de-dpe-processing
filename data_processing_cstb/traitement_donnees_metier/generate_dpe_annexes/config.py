0393fe475ee56d04b3062fb7d160d4a261f8739c
BREITWILLER <antoine.breitwiller@cstb.fr>
2021-09-08 15:06:00 +0200

    comment paths

--- data_processing_cstb/traitement_donnees_metier/generate_dpe_annexes/config.py
+++ data_processing_cstb/traitement_donnees_metier/generate_dpe_annexes/config.py
@@ -2,12 +2,12 @@ import os
 import logging
 
 config = dict()
-paths = {'DPE_DEPT_ANNEXE_PATH': os.environ.get('DPE_DEPT_ANNEXE_PATH', r'D:\data\dpe_full\annexes_cstb'),
-         'DPE_DEPT_PATH': os.environ.get('DPE_DEPT_PATH', r'D:\data\dpe_full\depts'),
-         'ES_SERVER_PATH': os.environ.get('ES_SERVER_PATH', os.environ[
-                      'USERPROFILE'] + r'\apps\elasticsearch-7.10.2-windows-x86_64\elasticsearch-7.10.2\bin\elasticsearch.bat'),
-
-         }
+#paths = {'DPE_DEPT_ANNEXE_PATH': os.environ.get('DPE_DEPT_ANNEXE_PATH', r'D:\data\dpe_full\annexes_cstb'),
+#         'DPE_DEPT_PATH': os.environ.get('DPE_DEPT_PATH', r'D:\data\dpe_full\depts'),
+#         'ES_SERVER_PATH': os.environ.get('ES_SERVER_PATH', os.environ[
+#                      'USERPROFILE'] + r'\apps\elasticsearch-7.10.2-windows-x86_64\elasticsearch-7.10.2\bin\elasticsearch.bat'),
+#
+#         }
 
 # logger
 FORMAT = "%(asctime)s %(name)s %(levelname)-8s %(message)s"
